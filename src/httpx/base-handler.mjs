0b3e65065f4b7c2a5cf7919e250d0cec37043c8c
Dat Dinhquoc <datdinhquoc@hotmail.com>
2022-12-10 01:53:05 +0700

    Base files

--- src/httpx/base-handler.mjs
+++ src/httpx/base-handler.mjs
@@ -1,1 +1,22 @@
+/**
+ * Base HTTP handler
+ */ 
+class base_handler {
+
+    /**
+     * Process GET
+     */ 
+    static handle_get(Req,Res){
+        // To override
+    }
+
+    /**
+     * Process POST
+     */ 
+    static handle_post(Req,Res){
+        // To override
+    }
+}
+
+export default base_handler;
 // EOF
--- src/httpx/base-server.mjs
+++ src/httpx/base-server.mjs
@@ -14,39 +14,106 @@ const loge = console.error;
  * Base server
  */ 
 class base_server {
-    Env_Name = null;
-    port     = null;
+    Cwd             = process.cwd();
+    Env_Name        = null;
+    port            = null;
+    Http_Server     = null;
+    Http2_Server    = null; // Unused, Chromium-based accepts HTTP2 secure only?
+    Handlers_Dir    = null;
+    Ws_Handlers_Dir = null;
 
     /**
-     * Ctor
+     * Ctor, config file and handlers dirs have paths starting from current working dir.
      */ 
-    constructor(Config_File){
-        this.Env_Name    = process.env.Envname;
-        this.Config_File = Config_File;        
+    constructor(Config_File,Handlers_Dir="handlers",Ws_Handlers_Dir="ws-handlers"){ 
+        this.Env_Name        = process.env.Envname;
+        this.Config_File     = Config_File;        
+        this.Handlers_Dir    = Handlers_Dir;
+        this.Ws_Handlers_Dir = Ws_Handlers_Dir;
+
+        // Scope
+        this.handle_request = this.handle_request.bind(this);
+    }
+
+    /**
+     * Process request 
+     */ 
+    async process_request(Req,Res,Handler_File){
+        log("Handler file:",Handler_File);
+        try{
+            var Handler = (await import(Handler_File)).default;
+        }
+        catch(Err){
+            log(`No handler for ${Req.method} ${Req.url}`);
+            Res.end(`No handler for ${Req.method} ${Req.url}`);
+            return;
+        }
+        
+        if (Req.method=="GET")
+            Handler.handle_get(Req,Res);
+        else
+        if (Req.method=="POST")
+            Handler.handle_post(Req,Res);
+        else{
+            log(`Method ${Req.method} is not supported.`);
+            Res.end();
+        }
+    }
+
+    /**
+     * Handle request
+     */
+    handle_request(Req,Res){ // Req:IncomingMessage, Res:ServerResponse
+        log(`\n${Req.method} ${Req.url}`);
+        var Path   = Req.url;
+        var Tokens = Path.split("/").filter(X => X.trim().length>0);
+
+        // Root
+        if (Path.trim()=="/"){
+            this.process_request(Req,Res, `${this.Cwd}/${this.Handlers_Dir}/root.mjs`);
+            return;
+        }
+
+        // Bad request
+        if (Tokens.length==0){
+            log("Bad request, empty path.");
+            Res.end();
+            return;
+        }
+
+        // Normal request
+        if (Path.charAt(0)=="/") Path=Path.slice(1);
+        this.process_request(Req,Res, `${this.Cwd}/${this.Handlers_Dir}/${Path}.mjs`);
     }
 
     /**
      * Init
      */ 
     async init(){
-        log("Envmnt:",this.Env_Name);
+        log("Env:",this.Env_Name);
         
         // Load config
         var Cwd         = process.cwd();
         var Config_Path = `${Cwd}/${this.Config_File}`;
         var Json        = await fs.promises.readFile(Config_Path);
         var Config      = JSON.parse(Json);
-        log("Config:",Config);
+        log("Config:",JSON.stringify(Config,null,4));
 
         // Set port
         this.port = Config.Ports[this.Env_Name];
-        log("Port:\x20\x20",this.port);
+        log("Port:",this.port);
     }
 
     /**
      * Start
      */ 
-    async start(){
+    start(){
+        var Http_Server  = http.createServer(this.handle_request);        
+        this.Http_Server = Http_Server;
+
+        // Start listening
+        this.Http_Server.listen(this.port);
+        log(`Server listening at port ${this.port}...`);
     }
 }
 
